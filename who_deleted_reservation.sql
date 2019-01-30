52a6f65f5f5ab3e876bab5c131511571d3efe6a2
AlexPanehnko <47169932+AlexPanehnko@users.noreply.github.com>
2019-01-30 16:08:56 +0300

    Исправления
    1) Убрал лишние колонки:
       tst.created_at, tst.comment
    2) Семантика

--- who_deleted_reservation.sql
+++ who_deleted_reservation.sql
@@ -1,13 +1,18 @@
-SELECT 
-    tst.teacher_id, tst.start_at, tst.created_at, tst.removed_at, tst.type, tst.comment,
-
+SELECT
+    tst.teacher_id, tst.start_at, tst.removed_at, tst.type,
     CASE
       WHEN tst.removed_at is NOT NULL THEN al.created_at
-    END AS AL_created_at, al.created_by_user_id
-
-FROM skyeng_timetable.teacher_single_time tst
-LEFT JOIN skyeng_timetable.action_log al 
-ON al.created_at = tst.removed_at
-
-WHERE tst.teacher_id=1867030
-AND tst.type="unavailable" AND tst.start_at LIKE  '2018-09-26%'
+    END AS AL_created_at, 
+    al.created_by_user_id AL_created_by_user_id, al.action_type AL_action_type, al.comment AL_comment
+FROM
+    skyeng_timetable.teacher_single_time tst
+LEFT JOIN
+    skyeng_timetable.action_log al 
+    ON
+        al.created_at = tst.removed_at
+WHERE
+    tst.type="unavailable" 
+                                                    AND
+                                                        tst.teacher_id=1867030
+                                                    AND
+                                                        tst.start_at LIKE  '2018-09-26%'
