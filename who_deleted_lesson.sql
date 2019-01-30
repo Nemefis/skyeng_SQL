c3a54061b21afdaba4deacf4bfda74d195f52531
AlexPanehnko <47169932+AlexPanehnko@users.noreply.github.com>
2019-01-30 12:16:12 +0300

    Замена названий таблиц
    C - Class
    AL - Action log
    CL - Class status

--- who_deleted_lesson.sql
+++ who_deleted_lesson.sql
@@ -1,11 +1,11 @@
 SELECT 
-  a.student_id, a.teacher_id, a.start_at, a.class_status_id, a.removed_at, 
-  b.created_at AL_created_at, b.action_type AL_action_type, b.created_by_user_id AL_created_by_user_id, 
-  c.id CS_id, c.status CS_status, c.created_by_user_id CS_created_by_user_id, c.comment CS_comment,
+  c.student_id, c.teacher_id, c.start_at, c.class_status_id, c.removed_at, 
+  al.created_at AL_created_at, al.action_type AL_action_type, al.created_by_user_id AL_created_by_user_id, 
+  /*cs.id CS_id,*/ cs.status CS_status, cs.created_by_user_id CS_created_by_user_id, cs.comment CS_comment,
   CASE
-    WHEN a.removed_at is NULL THEN c.id is NULL
-    ELSE b.created_at is NULL
-END 
+    WHEN c.removed_at is NULL THEN cs.id is NULL
+    ELSE al.created_at is NULL
+END AS CS_ID
   FROM 
     (SELECT * 
       FROM skyeng_timetable.class 
@@ -15,6 +15,6 @@ END
                                                                           and 
                                                                           start_at    LIKE  '2017-10-15%'
 
-    ) a 
-      LEFT JOIN skyeng_timetable.action_log b ON a.removed_at = b.created_at
-      LEFT JOIN skyeng_timetable.class_status c ON a.class_status_id=c.id
+    ) c 
+      LEFT JOIN skyeng_timetable.action_log al ON c.removed_at = al.created_at
+      LEFT JOIN skyeng_timetable.class_status cs ON c.class_status_id=cs.id
