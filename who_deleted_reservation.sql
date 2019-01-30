76608f2346f9dba6fdd9875152235fc615d7ec22
AlexPanehnko <47169932+AlexPanehnko@users.noreply.github.com>
2019-01-30 15:51:20 +0300

    Дополнения
    Добавил кейз на проверку ремувд_эт, подправил отображение полей и добавил критерий поиска по дате

--- who_deleted_reservation.sql
+++ who_deleted_reservation.sql
@@ -1,4 +1,13 @@
-SELECT tst.teacher_id, tst.start_at, tst.created_at, tst.removed_at, tst.type, tst.comment
-  FROM skyeng_timetable.teacher_single_time tst  
-  WHERE tst.teacher_id=1867030
-  AND tst.type="unavailable"
+SELECT 
+    tst.teacher_id, tst.start_at, tst.created_at, tst.removed_at, tst.type, tst.comment,
+
+    CASE
+      WHEN tst.removed_at is NOT NULL THEN al.created_at
+    END AS AL_created_at, al.created_by_user_id
+
+FROM skyeng_timetable.teacher_single_time tst
+LEFT JOIN skyeng_timetable.action_log al 
+ON al.created_at = tst.removed_at
+
+WHERE tst.teacher_id=1867030
+AND tst.type="unavailable" AND tst.start_at LIKE  '2018-09-26%'
